97f349860c867ed1ed1b952ff89cccc20a76204b
Dylan Ocampo <dylanomar@live.com.mx>
2024-12-19 16:29:10 -0600

    Button

--- src/App.js
+++ src/App.js
@@ -12,48 +12,21 @@ function App() {
   const urlEdit = "recsam://recsam.com/editProfile";
   const other = "recsam://recsam.com/login";
 
+  let destination = other;
+
+  if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Success"){
+    destination = urlSuccess
+  }
+
+  if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Cancel"){
+    destination = urlCancel
+  }
+
+  if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Edit"){
+    destination = urlEdit
+  }
+
   
-  function openLinkEffect() {
-    if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Success"){
-      window.location.replace(urlSuccess);
-    }
-
-    if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Cancel"){
-      window.location.replace(urlCancel);
-    }
-
-    if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Edit"){
-      window.location.replace(urlEdit);
-    }
-  }
-
-  function openLinkClick() {
-    if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Success"){
-      window.location.replace(urlSuccess);
-    }
-
-    if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Cancel"){
-      window.location.replace(urlCancel);
-    }
-
-    if(window.location.href === "https://recsamdeveloper.github.io/recsam/#Edit"){
-      window.location.replace(urlEdit);
-    }
-
-    window.location.replace(other);
-  }
-
-  setTimeout(() => {
-    openLinkEffect();
-  }, 2000);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      openLinkEffect();
-    }, 2000);
-
-    return () => clearTimeout(timeoutId);
-  },[])
 
   return (
     <div className='App'>
@@ -61,13 +34,13 @@ function App() {
       <div style={{minHeight: "40vh", minWidth: "100vw"}}>
       </div>
       <Cubes></Cubes>
+      <div className='ButtonStyle' onClick={() => window.open(destination)} ><p className='TextButton'>  Haz click aquí para regresar a la app →</p></div>
       <div className='TextContainer'><p className='Text'> Descarga la aplicación de RECSAM</p></div>
       <div className='AppContainer'>
 
         <Playstore></Playstore>
         <Appstore></Appstore>
       </div>
-      <div className='linkContainer'><p className='linkText'>Si ya tienes la app y no se te ha redirigido haz click <a onClick={openLinkClick} className='link'>aqui</a></p></div>
     </div>
   );
 }
